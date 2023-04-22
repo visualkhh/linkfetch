619487566514eda368a4c1723cdeb85274415e78
김현하(visual) <118410962+jaranda-visual@users.noreply.github.com>
2023-04-22 13:53:15 +0900

    initialize

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,45 @@ export type FetchDoc = { $ref: string };
 // export type FetchObjectPromiseType<T> = {
 //   [P in keyof T]: T[P] extends object ? FetchObjectPromiseType<T[P]>  : FetchFieldPromiseType<T[P]>;
 // }
-
 // type Capitalize<T> = T extends `${infer F}${infer R}` ? `${Uppercase<F>}${R}` : T;
-export type FetchFieldPromiseType<T> =  (config?: any) => Promise<T> ;
 // export type FetchObjectPromiseObjectType<T> = {
 //   [P in keyof T as `$${P}`]: T[P] extends object ? FetchObjectPromiseObjectType<T[P]>  : FetchFieldPromiseType<T[P]>;
 // }
 // [P in keyof T as `$${P}`]: FetchFieldPromiseType<T[P]>;
 
-export type FetchObjectPromiseType<T> = {
-  [P in keyof T as `${PrefixType}${P}`]: FetchFieldPromiseType<T[P]>;
+// export type FetchObjectPromiseType<T> = OptionalDeep<T> ;
+// {
+//   // [P in keyof T as `${PrefixType}${P}`]: T[P] extends object ? FetchFieldPromiseType<T[P]> : never;
+// } & {
+//   // [P in keyof T]?: T[P] extends object ? FetchObjectPromiseType<T[P]>  : T[P];
+// } & OptionalDeep<T> ;
+
+// type OptionalDeep<T> = {
+//   [P in keyof T]?: T[P] extends object ? OptionalDeep<T[P]> : T[P];
+// }
+// type Optional<T> = {
+//   [P in keyof T]?:  T[P];
+// }
+// type Capitalize<T> = T extends `${infer F}${infer R}` ? `${Uppercase<F>}${R}` : T;
+// export type FetchFieldPromiseType<T, C> =  (config?: C) => Promise<T> ;
+// export type FetchObjectPromiseObjectType<T> = {
+//   [P in keyof T as `$${P}`]: T[P] extends object ? FetchObjectPromiseObjectType<T[P]>  : FetchFieldPromiseType<T[P]>;
+// }
+// [P in keyof T as `$${P}`]: FetchFieldPromiseType<T[P]>;
+
+
+// export type FetchObjectPromiseType<T, C> = {
+//   [P in keyof T as `${PrefixType}${P}`]-?: T[P] extends object ? FetchFieldPromiseType<T[P], C> : never;
+// } & {
+//   [P in keyof T]?: T[P] extends object ? OptionalDeep<FetchObjectPromiseType<T[P], C>> : T[P];
+// };
+
+export type FetchFieldPromiseType<T, C> =  (config?: C) => Promise<T> ;
+export type FetchObjectPromiseType<T, C> = {
+  [P in keyof T as `$${P}`]: T[P] extends object ? FetchFieldPromiseType<T[P], C> : never;
 } & {
-  [P in keyof T]?: T[P] extends object ? FetchObjectPromiseType<T[P]>  : T[P];
-} ;
+  [P in keyof T]?: T[P] extends object ? FetchObjectPromiseType<T[P], C> : T[P];
+};;
 
 
 export type FetchFieldType<T> =  T ;
@@ -27,7 +53,7 @@ export type FetchObjectType<T> = {
   [P in keyof T]: T[P] extends object ? FetchObjectType<T[P]> | FetchDoc : FetchFieldType<T[P]>;
 }
 
-export const linkFetch = <T = any, D = FetchObjectType<T>>(docObject: D, fetch:(data: {origin: any, doc?: FetchDoc}, config: any) => Promise<any>): FetchObjectPromiseType<T> => {
+export const linkFetch = <T = any, C = any>(docObject: FetchObjectType<T>, fetch:(data: {origin?: any, doc?: FetchDoc}, config: C) => Promise<any>): FetchObjectPromiseType<T, C> => {
   const doc = Object.assign({}, docObject) as any;
   const proxy = (field: any) => {
     if ('_FetchProxy_isProxy' in field) {
--- test/index.ts
+++ test/index.ts
@@ -1,6 +1,18 @@
 import { FetchObjectType, linkFetch } from 'linkfetch';
 
-// export type FetchFieldType<T> =  T | Promise<T> | ((...arg: any[]) => Promise<T> | T) ;
+// }
+// type Optional<T> = {
+//   [P in keyof T]?:  T[P];
+// }
+// type OptionalDeep<T> = {
+//   [P in keyof T]?: T[P] extends object ? OptionalDeep<T[P]> : T[P];
+// }
+// export type FetchFieldPromiseType<T> =  (config?: any) => Promise<T> ;
+// export type FetchObjectPromiseType<T> = {
+//   [P in keyof T as `$${P}`]: T[P] extends object ? FetchFieldPromiseType<T[P]> : never;
+// } & {
+//   [P in keyof T]?: T[P] extends object ? FetchObjectPromiseType<T[P]> : T[P];
+// };;
 
 
 type Data = {
@@ -11,9 +23,27 @@ type Data = {
     name: string;
     good: {
       name: string;
+      addr:string;
     }
   }
 }
+// type ZZ = OptionalDeep<Data>;
+// const dd = {
+//
+// } as FetchObjectPromiseType<Data>;
+// const aaa = dd.name;
+// aaa?.length;
+// const wwww = dd.wow;
+// const z = dd.wow!;
+// dd.wow!.$good;
+// z.$good().then(it => it.name);
+// dd.$wow();
+
+
+type Config = {
+  name: string
+}
+
 (async () => {
 
   const data: FetchObjectType<Data> = {
@@ -28,23 +58,29 @@ type Data = {
     }
   }
 
-  const r = linkFetch<Data>(data, (data, config) => {
+  const r = linkFetch<Data, Config>(data, (data, config) => {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         resolve('xxxxx');
       }, 1000);
     });
   });
-  // console.log('----->', await r.name);
-  // console.log(r.name);
-  // r.friendsAA
-  const a = r.age;
-  const aa =11;
-  const zz = r.wow.good;
-  console.log(r.wow.good.name)
-  // r.wow.$good
-  let data1 = r.$wow();
-  console.log(data1)
+
+  // const za = await r.$wow().then(it => it.good.name);
+  // const aa = await r.wow.$good().then(it => it.name);
+
+
+  // // console.log('----->', await r.name);
+  // // console.log(r.name);
+  // // r.friendsAA
+  // const a = r.age;
+  // const aa =11;
+  // const zz = r;
+  // zz.name
+  // console.log(r.wow.good.name)
+  // // r.wow.$good
+  // let data1 = r.$wow();
+  // console.log(data1)
   // console.log(r.wow.then(it => it.good)good);
 
 })();
